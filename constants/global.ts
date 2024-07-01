d665969fabdd018b1a94f7081c0d58e60e8af847
hamzaODprogramer <hamzaouadoud116@gmail.com>
2024-07-01 22:14:03 +0100

    first commit

---
+++ constants/global.ts
@@ -0,0 +1,11 @@
+import Constants from "expo-constants";
+
+export const apiKey = Constants.expoConfig?.extra?.EXPO_PUBLIC_API_KEY
+
+export const apiUrl = Constants.expoConfig?.extra?.EXPO_PUBLIC_API_URL
+
+export const upload_perset = Constants.expoConfig?.extra?.EXPO_PUBLIC_UPLOAD_PERSET
+
+export const cloud_name = Constants.expoConfig?.extra?.EXPO_PUBLIC_CLOUD_NAME
+
+export const apiServerHandleUrl = Constants.expoConfig?.extra?.EXPO_PUBLIC_HANDLE_IMAGE_SERVER_URL
